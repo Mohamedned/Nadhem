c3e351b35728730ed5032d3911bd5ca12ee7e041
Mohamed Ali <mohrtd@gmail.com>
2024-02-18 15:34:31 +0100

    Voeg scrollView toe

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -27,11 +27,13 @@ export default function App() {
         <Button title='Voeg Toe' onPress={addGoalHandler}/>
       </View>
       <View style={styles.goelsContainer} >
+        <ScrollView alwaysBounceVertical={false}>
         {goals.map((goal, index) => (
           <View key={index} style={styles.goalItem} >
             <Text style={styles.goalText}>{goal}</Text>
           </View>
         ))}
+        </ScrollView>
       </View>
     </View>
   );
