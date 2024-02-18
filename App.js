18e2396e98cb6246b1d548143c62eb2b3bea3559
Mohamed Ali <mohrtd@gmail.com>
2024-02-18 14:59:01 +0100

    Voeg een state toe

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Hallo!</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+         style={styles.textInput} 
+         placeholder='Jouw course doel!' 
+         onChangeText={goalInputHandler}
+        />
+        <Button title='Voeg Toe' onPress={addGoalHandler}/>
+      </View>
+      <View style={styles.goelsContainer} >
+        <Text >Lijst van Doelen...</Text>
+      </View>
     </View>
   );
 }
@@ -11,8 +31,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput: {
+    borderWidth: 1, 
+    borderColor: '#cccccc', 
+    width: '70%',
+    marginRight: 8,
+    padding: 8
+  },
+  goelsContainer: {
+    flex: 5
+  }
 });
